b0ca2655e7640f86764108cc8e8a6ffc6ec2951f
Elnatan Teklu <elnatanteklu@gmail.com>
2019-02-23 13:41:44 -0500

    added completedPrograms and examplePrograms folders

---
+++ completedPrograms/basic/exampleProgram1/helloWorld.js
@@ -0,0 +1,25 @@
+/* 
+Author: 
+Description: display hello world
+*/
+
+
+// global variable using const
+const greeting = "hello world";
+
+
+
+// displays hello world as an alert popup in browser 
+alert(greeting);
+
+
+//displays hello world in console
+console.log(greeting);
+console.log(" ") // empty space used for formating 
+
+
+// using string literals 
+console.log(`using string literals ${greeting}`);
+
+
+
---
+++ completedPrograms/basic/exampleProgram2/console.js
@@ -0,0 +1,22 @@
+/*
+Author: 
+Description: show the different ways to use console  
+*/
+
+
+
+// displays hello world as an error message in console
+console.log(" "); // empty space just for formating 
+
+const errorMessage = "Opps something went wrong";
+console.error(errorMessage);
+
+console.log(" "); // empty space just for formating 
+
+
+
+// displays hello world as a warning message in console
+const warnMessage = "Hey be careful";
+console.warn(warnMessage);
+
+
