a6d499f5fdcad284ed569a72624d379cbb03713b
YujingGao <gao.yujing.csu@gmail.com>
2019-03-07 14:26:53 -0500

    testing git

--- completedPrograms/advanced/exampleProgram1/calculator.js
+++ completedPrograms/advanced/exampleProgram1/calculator.js
@@ -1,2 +1,30 @@
 console.log("hello world");
 alert("hello world");
+const "test";
+
+if (3 === 3) {
+    console.log(((("nested loop"))));
+    console.log("nested loop");
+    console.log("nested loop");
+
+    console.log("nested loop");
+    console.log("nested loop");
+    console.log("nested loop");
+    console.log("nested loop");
+    console.log("nested loop");
+
+
+
+} else {
+    console.log("nested loop");
+    console.log("nested loop");
+    console.log("nested loop");
+
+    console.log("nested loop");
+    console.log("nested loop");
+    console.log("nested loop");
+    console.log("nested loop");
+    console.log("nested loop");
+
+
+}
