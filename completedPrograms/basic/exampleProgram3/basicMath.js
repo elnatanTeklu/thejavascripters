dd4795784927bee1fcb6ecaf52da80cd99a60109
Elnatan Teklu <elnatanteklu@gmail.com>
2019-03-26 13:01:44 -0400

    updated basic program

---
+++ completedPrograms/basic/exampleProgram3/basicMath.js
@@ -0,0 +1,15 @@
+// Add 
+let addResult = 5000 + 100;
+console.log(`Addition : 5000 + 100 = ${addResult} \n`);
+
+// subtract
+let subResult = 5000 - 100;
+console.log(`Subtract : 5000 - 100 = ${subResult} \n`);
+
+// Multiplication 
+let mulResult = 5000 * 100;
+console.log(`Multiply : 5000 * 100 = ${mulResult} \n`);
+
+// Divide
+let divResult = 5000 / 100;
+console.log(`Divide : 5000 / 100 = ${divResult} \n`);
